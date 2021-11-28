7c4dd6cc693ad040671a954329f893bd3b8d5266
Udit Kumar <uditkumarussa@gmail.com>
2021-11-28 19:53:49 +0530

    removed: unused route

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const express = require("express");
 const connectDB = require("./db/connectDB");
 const cors = require("cors");
-const {
-  getUserImage,
-  updateUserImage,
-  getCurrentImage,
-} = require("./controllers/User");
+const { updateUserImage, getCurrentImage } = require("./controllers/User");
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 connectDB();
-
-app.get("/create-cover", getUserImage);
 
 app.get("/forceUpdateCover", updateUserImage);
 
