0acc5d155e448748946b905319a8d7bd63447d0d
Udit Kumar <55291327+uditkumar01@users.noreply.github.com>
2022-09-17 02:21:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   env: {
     MIN_CACHE_AGE_IN_SECS: process.env.MIN_CACHE_AGE_IN_SECS,
     MAX_CACHE_AGE_IN_SECS: process.env.MAX_CACHE_AGE_IN_SECS,
+    PATTERN_DIR_URL: process.env.PATTERN_DIR_URL,
   },
   images: {
     domains: [],
