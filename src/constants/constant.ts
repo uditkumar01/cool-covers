36f67edfbcf1b421b51cad23cbaf4f31128ba14e
Udit Kumar <55291327+uditkumar01@users.noreply.github.com>
2023-10-23 14:35:32 +0530

    Update constant.ts

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,4 +1,5 @@
 // const SVGS_BASE_PATH = path.join(IMAGES_BASE_PATH, "..", "svgs");
+const PATTERNS_AVAILABLE = ['p1','p2','p3','p4','p5','p6','p7','p8','naruto']
 const GOOGLE_FONT_NAMES = [
   "Poppins",
   "Quicksand",
@@ -8,5 +9,6 @@ const GOOGLE_FONT_NAMES = [
 ];
 
 export default {
-  GOOGLE_FONT_NAMES
+  GOOGLE_FONT_NAMES,
+  PATTERNS_AVAILABLE
 };
