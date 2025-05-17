38f727f7c013a73b9a01976917506823718de98a
Udit Kumar <uditkumarussa@gmail.com>
2025-05-18 00:23:08 +0530

    Add `pnpm-lock.yaml` to lock dependencies versions

--- src/utils/getUsers.ts
+++ src/utils/getUsers.ts
@@ -1,36 +1,36 @@
-import JSSoup from "jssoup";
-import getImagesFromLink from "./getImagesFromLink";
+import JSSoup from 'jssoup';
+import getImagesFromLink from './getImagesFromLink';
 
 export interface IFollower {
-  avatar_url: string;
-  login: string;
+	avatar_url: string;
+	login: string;
 }
 
 const getUsers = async (html: any, limit: number) => {
-  let soup = new JSSoup(html);
-  const imgTags = soup.findAll("img") || [];
-  const data: IFollower[] = [];
+	let soup = new JSSoup(html);
+	const imgTags = soup.findAll('img') || [];
+	const data: IFollower[] = [];
 
-  for (let i = 0; data.length < limit && i < imgTags.length; i++) {
-    const attrs = imgTags[i].attrs;
+	for (let i = 0; data.length < limit && i < imgTags.length; i++) {
+		const attrs = imgTags[i].attrs;
+    console.log(new URL(attrs.src).hostname)
+		if (
+			attrs.src &&
+			attrs.alt &&
+			attrs.height === '65' &&
+			attrs.width === '65'
+		) {
+			const imageSource = attrs.src.replace('s=100&amp;', '');
+			const login = attrs.alt.split('@')[1];
+			const imgBuf = (await getImagesFromLink(imageSource)) || '';
+			data.push({
+				avatar_url: imageSource ? imgBuf : '',
+				login,
+			});
+		}
+	}
 
-    if (
-      attrs.src &&
-      attrs.alt &&
-      attrs.height === "50" &&
-      attrs.width === "50"
-    ) {
-      const imageSource = attrs.src.replace("s=100&amp;", "");
-      const login = attrs.alt.split("@")[1];
-      const imgBuf = (await getImagesFromLink(imageSource)) || "";
-      data.push({
-        avatar_url: imageSource ? imgBuf : "",
-        login,
-      });
-    }
-  }
-
-  return { followers: data };
+	return { followers: data };
 };
 
 export default getUsers;
